18d9723b21e6adcc2b495d7a3cacdb353a1623d0
zylan18 <zylan18@github.com>
2023-09-19 20:38:21 +0530

    added eureka.instance.hostname=localhost for api-gateway to work locally

--- src/main/java/com/telusko/quizservice/service/QuizService.java
+++ src/main/java/com/telusko/quizservice/service/QuizService.java
@@ -40,30 +40,16 @@ public class QuizService {
 	}
 
 	public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(Integer id) {
-//		Optional<Quiz> quiz=quizDao.findById(id);
-//		List<Question> questonFromDB=quiz.get().getQuestions();
-		List<QuestionWrapper> questionsForUser=new ArrayList<>();
-//		
-//		for(Question q : questonFromDB) {
-//			QuestionWrapper qw=new QuestionWrapper(q.getId(), q.getQuestionTitle(), q.getOption1(), q.getOption2(), q.getOption3(), q.getOption4());
-//			questionsForUser.add(qw);
-//		}
-		
-		return new ResponseEntity<>(questionsForUser,HttpStatus.OK);
+		Quiz quiz=quizDao.findById(id).get();
+		List<Integer> questionIds=quiz.getQuestionsIds();
+		ResponseEntity<List<QuestionWrapper>>questions=quizInterface.getQuestionsFromId(questionIds);
+		return (questions);
 	}
 
 	public ResponseEntity<Integer> calculateResult(Integer id, List<Response> responses) {
-		Quiz quiz = quizDao.findById(id).get(); 
-//		List<Question>questions=quiz.getQuestions();
-		int right=0;
-//		int i=0;
-//		for(Response response:responses) {
-//			if(response.getResponse().equals(questions.get(i).getRightAnswer())) {
-//				right++;
-//			}
-//			i++;
-//		}
-		return new ResponseEntity<>(right,HttpStatus.OK);
+		ResponseEntity<Integer>score= quizInterface.getScore(responses);
+		return score;
+		
 	}
 	
 	
